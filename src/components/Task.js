f37780bf741f353ca37c6c5679723ddc617e5841
arthurpsribeiro <arthurpsribeiro@gmail.com>
2021-06-25 12:24:18 -0300

    inicio lista e tarefa

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,68 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import moment from "moment";
+import "moment/locale/pt-br";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default (props) => {
-	return <View style={styles.container}></View>;
+	const TODAY = moment(props.estimateAt)
+		.locale("pt-br")
+		.format("ddd, D [de] MMMM");
+	const formatatedDate = moment;
+
+	function getCheckView(doneAt) {
+		if (doneAt) {
+			return (
+				<View style={styles.done}>
+					<Icon name="check" size={20} color="#fff" />
+				</View>
+			);
+		}
+		return <View style={styles.pending}></View>;
+	}
+
+	return (
+		<View style={styles.container}>
+			<TouchableWithoutFeedback>
+				<View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
+			</TouchableWithoutFeedback>
+			<View>
+				<Text style={styles.desc}>{props.desc}</Text>
+				<Text style={styles.date}>{TODAY}</Text>
+			</View>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-	container: {},
+	container: {
+		flexDirection: "row",
+		borderColor: "#aaa",
+		borderBottomWidth: 1,
+		alignItems: "center",
+		padding: 10,
+		backgroundColor: "#fff",
+	},
+	checkContainer: {
+		width: "20%",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	pending: {
+		height: 25,
+		width: 25,
+		borderRadius: 13,
+		borderWidth: 1,
+		borderColor: "#555",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	done: {
+		height: 25,
+		width: 25,
+		borderRadius: 13,
+		backgroundColor: "#4d7031",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
--- src/views/TaskList.js
+++ src/views/TaskList.js
@@ -1,15 +1,113 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+	View,
+	Text,
+	StyleSheet,
+	ImageBackground,
+	TouchableOpacity,
+	FlatList,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import Constants from "expo-constants";
+import moment from "moment";
+import "moment/locale/pt-br";
 
-const initialState = {};
+import commomStyles from "../commomStyles";
+import todayImage from "../../assets/imgs/today.jpg";
+import Task from "../components/Task";
+
+const initialState = {
+	showDoneTasks: true,
+	showAddTask: false,
+	visibleTasks: [],
+	tasks: [],
+};
 export default class TaskList extends Component {
 	state = { ...initialState };
 
 	render() {
-		return <View style={styles.container}></View>;
+		const TODAY = moment().locale("pt-br").format("LL");
+		return (
+			<View style={styles.container}>
+				<ImageBackground source={todayImage} style={styles.background}>
+					<View style={styles.iconBar}>
+						<TouchableOpacity>
+							<Icon
+								name={this.state.showDoneTasks ? "eye" : "eye-slash"}
+								size={30}
+								color={commomStyles.colors.secondary}
+							/>
+						</TouchableOpacity>
+					</View>
+					<View style={styles.titleBar}>
+						<Text style={styles.title}>Hoje</Text>
+						<Text style={styles.subtitle}>{TODAY}</Text>
+					</View>
+				</ImageBackground>
+				<View style={styles.containerList}>
+					<Task
+						desc="Assistir Filme"
+						estimateAt={new Date()}
+						doneAt={new Date()}
+					/>
+					<Task desc="Lavar Roupa" estimateAt={new Date()} />
+					<Task desc="Estudar" estimateAt={new Date()} doneAt={new Date()} />
+
+					{/* <FlatList
+						data={this.state.visibleTasks}
+						keyExtractor={(item) => item.id}
+						renderItem={Component}
+					/> */}
+				</View>
+				<TouchableOpacity style={styles.addButton}>
+					<Icon name="plus" size={30} color={commomStyles.colors.secondary} />
+				</TouchableOpacity>
+			</View>
+		);
 	}
 }
 
 const styles = StyleSheet.create({
-	container: {},
+	container: {
+		flex: 1,
+	},
+	background: {
+		flex: 3,
+	},
+	containerList: {
+		flex: 7,
+	},
+	iconBar: {
+		flexDirection: "row",
+		justifyContent: "flex-end",
+		marginHorizontal: 30,
+		marginTop: Constants.statusBarHeight,
+	},
+	titleBar: {
+		flex: 1,
+		justifyContent: "flex-end",
+	},
+	title: {
+		color: commomStyles.colors.secondary,
+		fontSize: 50,
+		marginLeft: 20,
+		marginBottom: 20,
+	},
+	subtitle: {
+		color: commomStyles.colors.secondary,
+		fontSize: 20,
+		marginLeft: 20,
+		marginBottom: 20,
+	},
+	addButton: {
+		backgroundColor: commomStyles.colors.today,
+		width: 80,
+		height: 80,
+		justifyContent: "center",
+		alignItems: "center",
+		position: "absolute",
+		right: 30,
+		bottom: 30,
+		borderRadius: 40,
+	},
 });
